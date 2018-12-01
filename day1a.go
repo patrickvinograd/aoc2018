3931ab378a26f603654a0fe3f11846288a86a5ac
Patrick Vinograd <patrick@adhocteam.us>
2018-11-30 22:52:40 -0800

    gofmt

--- day1a.go
+++ day1a.go
@@ -1,42 +1,41 @@
 package main
 
 import (
-  "fmt"
-  "strconv"
+	"fmt"
+	"strconv"
 )
 
 func readInput() []int {
-  result := make([]int, 0)
-  var in string
-  lines, _ := fmt.Scanln(&in)
-  for lines > 0 {
-    freq, _ := strconv.Atoi(in)
-    result = append(result, freq)
-    lines, _ = fmt.Scanln(&in)
-  }
-  return result
+	result := make([]int, 0)
+	var in string
+	lines, _ := fmt.Scanln(&in)
+	for lines > 0 {
+		freq, _ := strconv.Atoi(in)
+		result = append(result, freq)
+		lines, _ = fmt.Scanln(&in)
+	}
+	return result
 }
 
 func findRepeat(values []int) int {
-  seen := make(map[int]bool)
-  counter, i := 0, 0
-  for {
-    counter += values[i]
-    if seen[counter] == true {
-      return counter
-    } else {
-      seen[counter] = true
-    }
-    i++
-    if (i >= len(values)) {
-      i = 0
-    }
-  }
+	seen := make(map[int]bool)
+	counter, i := 0, 0
+	for {
+		counter, i = counter+values[i], i+1
+		if seen[counter] == true {
+			return counter
+		} else {
+			seen[counter] = true
+		}
+		if i >= len(values) {
+			i = 0
+		}
+	}
 }
 
 func main() {
-  values := readInput()
-  //fmt.Println(values)
-  answer := findRepeat(values)
-  fmt.Println(answer)
+	values := readInput()
+	//fmt.Println(values)
+	answer := findRepeat(values)
+	fmt.Println(answer)
 }
